b23fa2d3c0e2e2bcf95d9cdd309ca7f81522aba7
Alexander Neumann <alexander@bumpern.de>
2019-02-01 20:51:46 +0100

    Add minimal user interface

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/happal/osmosis
 go 1.12
 
 require (
+	github.com/spf13/pflag v1.0.3
 	golang.org/x/net v0.0.0-20190125091013-d26f9f9a57f3
 	golang.org/x/text v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,27 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/happal/osmosis/certauth"
 	"github.com/happal/osmosis/proxy"
+	"github.com/spf13/pflag"
 )
 
-const (
-	cacert = "ca.crt"
-	cakey  = "ca.key"
-)
+// Options collects global settings.
+type Options struct {
+	CertificateFilename, KeyFilename string
+	Listen                           string
+}
+
+var opts Options
+
+func init() {
+	fs := pflag.NewFlagSet("osmosis", pflag.ExitOnError)
+	fs.StringVar(&opts.CertificateFilename, "cert", "ca.crt", "read certificate from `file`")
+	fs.StringVar(&opts.KeyFilename, "key", "ca.key", "read private key from `file`")
+	fs.StringVar(&opts.Listen, "listen", "[::1]:8080", "listen at `addr`")
+
+	err := fs.Parse(os.Args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error parsing flags: %v\n", err)
+		os.Exit(1)
+	}
+}
 
 func main() {
-	ca, err := certauth.Load(cacert, cakey)
+	ca, err := certauth.Load(opts.CertificateFilename, opts.KeyFilename)
 	if os.IsNotExist(err) {
+		fmt.Printf("generate new CA certificate\n")
 		ca, err = certauth.NewCA()
 		if err != nil {
 			panic(err)
 		}
 
-		err = ca.Save(cacert, cakey)
+		err = ca.Save(opts.CertificateFilename, opts.KeyFilename)
 		if err != nil {
 			panic(err)
 		}
@@ -29,6 +48,6 @@ func main() {
 
 	log.Printf("CA loaded: %v\n", ca.Certificate.Subject)
 
-	proxy := proxy.New("[::1]:9090", ca)
+	proxy := proxy.New(opts.Listen, ca)
 	log.Fatal(proxy.Serve())
 }
