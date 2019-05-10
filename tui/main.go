6c3151b5e2a1a028dfb04d76c0ff5b8678d1eb71
Erik Geiser <erik.geiser@posteo.net>
2019-05-10 18:26:14 +0200

    Refactor gui and add request/response view

--- tui/main.go
+++ tui/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -20,6 +21,138 @@ type Request struct {
 	ID int
 	*http.Request
 	*http.Response
+}
+
+// OsmosisGui is a container through which the elements to "see" each other
+type OsmosisGui struct {
+	app               *tview.Application
+	pages             *tview.Pages
+	requests          []Request
+	requestViewEvents chan Request
+	statusBarEvents   chan string
+}
+
+// NewOsmosisGui returns a new GUI container
+func NewOsmosisGui() *OsmosisGui {
+	g := &OsmosisGui{
+		app:               tview.NewApplication(),
+		pages:             tview.NewPages(),
+		requestViewEvents: make(chan Request),
+		statusBarEvents:   make(chan string),
+	}
+	g.pages.AddPage("overview", g.overviewPage(), true, true)
+	g.pages.AddPage("viewer", g.viewerPage(), true, false)
+	g.app.SetRoot(g.pages, true).SetFocus(g.pages)
+	return g
+}
+
+// viewerPage create a page visualizing a single request
+func (g *OsmosisGui) viewerPage() *tview.Grid {
+	grid := tview.NewGrid().SetColumns(-1, -1).SetRows(-1, 3)
+	requestField := tview.NewTextView()
+	requestField.SetBorder(true).SetTitle("Request")
+	responseField := tview.NewTextView()
+	responseField.SetBorder(true).SetTitle("Response")
+	grid.AddItem(requestField, 0, 0, 1, 1, 0, 0, false)
+	grid.AddItem(responseField, 0, 1, 1, 1, 0, 0, false)
+	grid.AddItem(
+		tview.NewButton("back").SetSelectedFunc(func() {
+			g.pages.SwitchToPage("overview")
+		}), 1, 0, 1, 2, 0, 0, true)
+
+	// set the view content if a request ist selected
+	go func() {
+		for {
+			event := <-g.requestViewEvents
+			req, err := httputil.DumpRequest(event.Request, true)
+			if err != nil {
+				req = []byte(err.Error())
+			}
+			res, err := httputil.DumpResponse(event.Response, true)
+			if err != nil {
+				res = []byte(err.Error())
+			}
+			requestField.SetText(string(req))
+			responseField.SetText(string(res))
+		}
+	}()
+	return grid
+}
+
+// overviewPage creates a page containing the requests list
+func (g *OsmosisGui) overviewPage() *tview.Grid {
+	grid := tview.NewGrid().SetRows(-6, -1, 1)
+
+	table := g.requestsTable()
+	grid.AddItem(table, 0, 0, 1, 1, 0, 0, true)
+
+	lv := g.logView()
+	grid.AddItem(lv, 1, 0, 1, 1, 0, 0, false)
+
+	statusBar := tview.NewTextView()
+	go func() {
+		for {
+			statusBar.SetText(<-g.statusBarEvents)
+		}
+	}()
+	g.statusBarEvents <- "foo | text"
+	grid.AddItem(statusBar, 2, 0, 1, 1, 0, 0, false)
+	return grid
+}
+
+// requestsTable creates the list of requests within the overview page
+func (g *OsmosisGui) requestsTable() *tview.Table {
+	reqs, err := loadRequests(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	g.requests = reqs
+
+	table := tview.NewTable()
+	table.SetSelectedFunc(func(row int, column int) {
+		g.requestViewEvents <- g.requests[row-1]
+		g.pages.SwitchToPage("viewer")
+	})
+	// make first column and row a header which is always shown
+	table.SetFixed(1, 1)
+	// make rows selectable
+	table.SetSelectable(true, false)
+
+	table.SetBorder(true)
+
+	// header
+	table.SetCell(0, 0, &tview.TableCell{NotSelectable: true, Text: "[white::b]ID"})
+	table.SetCell(0, 1, &tview.TableCell{NotSelectable: true, Text: "[white::b]Host"})
+	table.SetCell(0, 2, &tview.TableCell{NotSelectable: true, Text: "[white::b]Method"})
+	table.SetCell(0, 3, &tview.TableCell{NotSelectable: true, Text: "[white::b]Status"})
+	table.SetCell(0, 4, &tview.TableCell{NotSelectable: true, Text: "[white::b]Params",
+		Expansion: 1})
+
+	for i, req := range g.requests {
+		table.SetCell(i+1, 0, &tview.TableCell{
+			NotSelectable: true,
+			Color:         tcell.ColorGreen,
+			Text:          fmt.Sprintf("%d", req.ID),
+		})
+		table.SetCell(i+1, 1, &tview.TableCell{
+			Color: tcell.ColorWhite,
+			Text:  req.URL.Scheme + "://" + req.Host,
+		})
+		table.SetCell(i+1, 2, &tview.TableCell{
+			Color: tcell.ColorWhite,
+			Text:  req.Method,
+		})
+		table.SetCell(i+1, 3, &tview.TableCell{
+			Color: tcell.ColorWhite,
+			Text:  printStatus(req.Response.StatusCode),
+		})
+		table.SetCell(i+1, 4, &tview.TableCell{
+			Color: tcell.ColorWhite,
+			Text:  "[white]" + printPathQuery(req.URL),
+		})
+	}
+
+	return table
 }
 
 func readRequest(dir string, id int) (*http.Request, error) {
@@ -110,17 +243,17 @@ func printPathQuery(u *url.URL) string {
 	url.Host = ""
 	return url.String()
 
-	buf := &strings.Builder{}
-	buf.WriteString(u.EscapedPath())
-	if u.ForceQuery || u.RawQuery != "" {
-		buf.WriteByte('?')
-		buf.WriteString(u.RawQuery)
-	}
-	if u.Fragment != "" {
-		buf.WriteByte('#')
-		buf.WriteString(u.Fragment)
-	}
-	return buf.String()
+	// buf := &strings.Builder{}
+	// buf.WriteString(u.EscapedPath())
+	// if u.ForceQuery || u.RawQuery != "" {
+	// 	buf.WriteByte('?')
+	// 	buf.WriteString(u.RawQuery)
+	// }
+	// if u.Fragment != "" {
+	// 	buf.WriteByte('#')
+	// 	buf.WriteString(u.Fragment)
+	// }
+	// return buf.String()
 }
 
 func printStatus(code int) string {
@@ -138,80 +271,6 @@ func printStatus(code int) string {
 	return fmt.Sprintf("[%s]%d[default]", color, code)
 }
 
-func requestsTable() tview.Primitive {
-	requests, err := loadRequests(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
-
-	table := tview.NewTable()
-	// make first column and row a header which is always shown
-	table.SetFixed(1, 1)
-	// make rows selectable
-	table.SetSelectable(true, false)
-
-	table.SetBorder(true)
-
-	// header
-	table.SetCell(0, 0, &tview.TableCell{
-		Color:         tcell.ColorWhite,
-		Attributes:    tcell.AttrBold,
-		NotSelectable: true,
-		Text:          "ID",
-	})
-	table.SetCell(0, 2, &tview.TableCell{
-		Color:         tcell.ColorWhite,
-		Attributes:    tcell.AttrBold,
-		NotSelectable: true,
-		Text:          "Host",
-	})
-	table.SetCell(0, 3, &tview.TableCell{
-		Color:         tcell.ColorWhite,
-		Attributes:    tcell.AttrBold,
-		NotSelectable: true,
-		Text:          "Method",
-	})
-	table.SetCell(0, 4, &tview.TableCell{
-		Color:         tcell.ColorWhite,
-		Attributes:    tcell.AttrBold,
-		NotSelectable: true,
-		Text:          "Status",
-	})
-	table.SetCell(0, 5, &tview.TableCell{
-		Color:         tcell.ColorWhite,
-		Attributes:    tcell.AttrBold,
-		NotSelectable: true,
-		Text:          "Params",
-		Expansion:     1,
-	})
-
-	for i, req := range requests {
-		table.SetCell(i+1, 0, &tview.TableCell{
-			NotSelectable: true,
-			Color:         tcell.ColorGreen,
-			Text:          fmt.Sprintf("%d", req.ID),
-		})
-		table.SetCell(i+1, 2, &tview.TableCell{
-			Color: tcell.ColorWhite,
-			Text:  req.URL.Scheme + "://" + req.Host,
-		})
-		table.SetCell(i+1, 3, &tview.TableCell{
-			Color: tcell.ColorWhite,
-			Text:  req.Method,
-		})
-		table.SetCell(i+1, 4, &tview.TableCell{
-			Color: tcell.ColorWhite,
-			Text:  printStatus(req.Response.StatusCode),
-		})
-		table.SetCell(i+1, 5, &tview.TableCell{
-			Color: tcell.ColorWhite,
-			Text:  printPathQuery(req.URL),
-		})
-	}
-
-	return table
-}
-
 var logger io.Writer
 
 func log(msg string, args ...interface{}) {
@@ -221,30 +280,18 @@ func log(msg string, args ...interface{}) {
 	fmt.Fprintf(logger, msg, args...)
 }
 
-func logView(app *tview.Application) tview.Primitive {
+func (g *OsmosisGui) logView() tview.Primitive {
 	tv := tview.NewTextView()
 	tv.SetBorder(true)
 	tv.SetChangedFunc(func() {
-		app.Draw()
+		g.app.Draw()
 	})
 	logger = tv
 	return tv
 }
 
 func main() {
-	app := tview.NewApplication()
-
-	grid := tview.NewGrid().SetRows(-6, -1, 1)
-
-	table := requestsTable()
-	grid.AddItem(table, 0, 0, 1, 1, 0, 0, true)
-
-	lv := logView(app)
-	grid.AddItem(lv, 1, 0, 1, 1, 0, 0, false)
-
-	status := tview.NewTextView()
-	status.SetText("status text")
-	grid.AddItem(status, 2, 0, 1, 1, 0, 0, false)
+	gui := NewOsmosisGui()
 
 	go func() {
 		for {
@@ -252,9 +299,7 @@ func main() {
 			log("foobar %v", time.Now())
 		}
 	}()
-
-	app.SetRoot(grid, true)
-	err := app.Run()
+	err := gui.app.Run()
 	if err != nil {
 		panic(err)
 	}
